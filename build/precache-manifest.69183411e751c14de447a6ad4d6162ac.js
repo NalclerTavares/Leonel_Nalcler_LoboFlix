ef22788c545b232a62d8760a91cd92d33d83f157
NalclerTavares <61871779+NalclerTavares@users.noreply.github.com>
2020-12-11 00:58:38 -0300

    Add files via upload

---
+++ build/precache-manifest.69183411e751c14de447a6ad4d6162ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0acf0c6aca4049ed6f5960def7bf4cd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43ee9d099fb710cc95b9",
+    "url": "/static/css/main.33ee89e7.chunk.css"
+  },
+  {
+    "revision": "2eb3be22d50737eda721",
+    "url": "/static/js/2.9ebe3ebb.chunk.js"
+  },
+  {
+    "revision": "75304a605f9c03e664948edc1c5186bb",
+    "url": "/static/js/2.9ebe3ebb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43ee9d099fb710cc95b9",
+    "url": "/static/js/main.0c74b3b8.chunk.js"
+  },
+  {
+    "revision": "56f07968ca5d5ac127a1",
+    "url": "/static/js/runtime-main.4a4591a3.js"
+  },
+  {
+    "revision": "c58285c75ebce03fb13d541485c8328a",
+    "url": "/static/media/logo.c58285c7.png"
+  }
+]);
